ba6945a71fabc7916adaceb7cecc16a849e31cee
James Colairo <jcolairo@spartaglobal.co>
2017-06-01 15:30:49 +0100

    setup menu pages with sidebar

--- frontend/js/router/app.js
+++ frontend/js/router/app.js
@@ -28,21 +28,41 @@ function MainRouter ($stateProvider, $urlRouterProvider, $locationProvider) {
       url: '/order',
       templateUrl: '/states/partials/order/order.html'
     })
+    .state('menu', {
+      url: '/menu',
+      templateUrl: '/states/partials/menu/menu.html'
+    })
     .state('breakfast', {
       url: '/menu/breakfast',
-      templateUrl: '/states/partials/menu/breakfast.html'
+      views: {
+        '': {templateUrl: '/states/partials/template.html'},
+        'aside@breakfast': {templateUrl: '/states/partials/menu/aside.html'},
+        'menu@breakfast': {templateUrl: '/states/partials/menu/breakfast.html'}
+      }
     })
     .state('lunch', {
       url: '/menu/lunch',
-      templateUrl: '/states/partials/menu/lunch.html'
+      views: {
+        '': {templateUrl: '/states/partials/template.html'},
+        'aside@lunch': {templateUrl: '/states/partials/menu/aside.html'},
+        'menu@lunch': {templateUrl: '/states/partials/menu/lunch.html'}
+      }
     })
     .state('afternoonTea', {
       url: '/menu/afternoonTea',
-      templateUrl: '/states/partials/menu/afternoonTea.html'
+      views: {
+        '': {templateUrl: '/states/partials/template.html'},
+        'aside@afternoonTea': {templateUrl: '/states/partials/menu/aside.html'},
+        'menu@afternoonTea': {templateUrl: '/states/partials/menu/afternoonTea.html'}
+      }
     })
     .state('canapes', {
       url: '/menu/canapes',
-      templateUrl: '/states/partials/menu/canapes.html'
+      views: {
+        '': {templateUrl: '/states/partials/template.html'},
+        'aside@canapes': {templateUrl: '/states/partials/menu/aside.html'},
+        'menu@canapes': {templateUrl: '/states/partials/menu/canapes.html'}
+      }
     });
 
   // $locationProvider.html5Mode(true);
