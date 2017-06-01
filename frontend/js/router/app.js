0312595610e384837c75649e0c548b57825cb481
James Colairo <jcolairo@spartaglobal.co>
2017-06-01 14:25:11 +0100

    setup for pages with html and states

--- frontend/js/router/app.js
+++ frontend/js/router/app.js
@@ -15,6 +15,34 @@ function MainRouter ($stateProvider, $urlRouterProvider, $locationProvider) {
     .state('signup', {
       url: '/signup',
       templateUrl: '/states/partials/auth/signup.html'
+    })
+    .state('about', {
+      url: '/about',
+      templateUrl: '/states/partials/about/about.html'
+    })
+    .state('contact', {
+      url: '/contact',
+      templateUrl: '/states/partials/contact/contact.html'
+    })
+    .state('order', {
+      url: '/order',
+      templateUrl: '/states/partials/order/order.html'
+    })
+    .state('breakfast', {
+      url: '/menu/breakfast',
+      templateUrl: '/states/partials/menu/breakfast.html'
+    })
+    .state('lunch', {
+      url: '/menu/lunch',
+      templateUrl: '/states/partials/menu/lunch.html'
+    })
+    .state('afternoonTea', {
+      url: '/menu/afternoonTea',
+      templateUrl: '/states/partials/menu/afternoonTea.html'
+    })
+    .state('canapes', {
+      url: '/menu/canapes',
+      templateUrl: '/states/partials/menu/canapes.html'
     });
 
   // $locationProvider.html5Mode(true);
