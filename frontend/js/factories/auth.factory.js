fd40beb9bd3228abadbf596557a53f1aed944fde
Niall Wallace <nj.wallace@outlook.com>
2017-07-07 19:20:33 +0100

    removed commented code

--- frontend/js/factories/auth.factory.js
+++ frontend/js/factories/auth.factory.js
@@ -17,10 +17,6 @@ function AuthFactory($firebaseAuth) {
 
   var fb = $firebaseAuth();
 
-  // fb.isAdmin = function () {
-  //   var user = this.$getAuth()
-  //   console.log('isAdmin: ', user)
-  // }
   return fb;
 }
 
