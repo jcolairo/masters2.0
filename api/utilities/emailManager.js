0d3af32b5af26aa55c51bcccdf70de16f056489e
James Colairo <jcolairo@spartaglobal.co>
2017-07-10 18:19:46 +0100

    got order to format properly; added multiple products to an order and sent email to client and owner :)

--- api/utilities/emailManager.js
+++ api/utilities/emailManager.js
@@ -2,9 +2,6 @@ function sendOrderConfirmation (user) {
   console.log('sending email to: ', user.email);
 
   var products = user.basket.items;
-  // console.log('mmmmmm********************************mmmm', products);
-  // console.log('mmmmmm********************************PRODUCT', products[0].product);
-  // console.log('mmmmmm********************************QTY', products[0].qty);
 
   var productsHTML = '';
 
@@ -13,26 +10,23 @@ function sendOrderConfirmation (user) {
   });
 
   function generateSingleProductHTML(prod) {
+
     return [
       '<p>',
-      '<b>' + prod + '</b>',
-      '</p>'
+      '<b>' + 'Title:' + '</b>'+ prod.product.title ,
+      '</p>',
+      '<p>',
+      '<b>' + 'Price:'+ '</b>' + prod.product.price,
+      '</p>',
+      '<p>',
+      '<b>' + 'Description:' + '</b>'+ prod.product.description,
+      '</p>',
+      '<p>',
+      '<b>' + 'QTY:' + '</b>'+ prod.qty,
+      '</p>',
+      '<br>'
     ].join('');
   }
-  // console.log('******************generateSingleProductHTML()', generateSingleProductHTML(products));
-
-  // for (var i = 0; i < user.basket.items.length; i++) {
-  //   console.log('***************************************************************************', user.basket.items[i]);
-  // }
-
-  // var basket = user.basket.items[0];
-  // var title = basket.product.title;
-  // var price = basket.product.price;
-  // var description = basket.product.description;
-  // var qty = basket.qty;
-  // var comments = basket.customer_notes;
-  //
-  // var products = 'Tittie: '+ title +  '<br>' + 'Price' + price + '<br>' + 'Description:' + description + '<br>' + 'Quantity:' + qty + '<br>' + 'Comments:' + comments;
 
   // all sendgrid code here
 
@@ -42,8 +36,7 @@ function sendOrderConfirmation (user) {
   var fromEmail = new helper.Email('jamescolairo37@gmail.com');
   var toEmail = new helper.Email(user.email);
   var subject = 'Order Confirmation';
-  var content = new helper.Content('text/html', products);
-  // var contentPrice = new helper.Content('text/plain', JSON.stringify(products));
+  var content = new helper.Content('text/html', productsHTML);
   var mail = new helper.Mail(fromEmail, subject, toEmail, content);
 
   var sg = require('sendgrid')(process.env.MASTERS_SENDGRIB);
@@ -60,10 +53,30 @@ function sendOrderConfirmation (user) {
     console.log(response.statusCode);
     console.log(response.body);
     console.log(response.headers);
-    // console.log('******************************', user.basket.items);
   });
 
+  helper = require('sendgrid').mail;
+  fromEmail = new helper.Email('jamescolairo37@gmail.com');
+  toEmail = new helper.Email('jamescolairo37@gmail.com');
+  subject = 'Order Confirmation';
+  content = new helper.Content('text/html', productsHTML);
+  mail = new helper.Mail(fromEmail, subject, toEmail, content);
 
+  sg = require('sendgrid')(process.env.MASTERS_SENDGRIB);
+  request = sg.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: mail.toJSON()
+  });
+
+  sg.API(request, function (error, response) {
+    if (error) {
+      console.log('Error response received');
+    }
+    console.log(response.statusCode);
+    console.log(response.body);
+    console.log(response.headers);
+  });
 }
 
 
