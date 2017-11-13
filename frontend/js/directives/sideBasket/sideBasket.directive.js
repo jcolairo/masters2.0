55993b72ec25aab95f71466ad3f5252a2f5b3553
James Colairo <jamescolairo37@gmail.com>
2017-11-13 10:58:29 +0000

    added sideBar cart directive

---
+++ frontend/js/directives/sideBasket/sideBasket.directive.js
@@ -0,0 +1,37 @@
+
+function basketTotal (userInfoService) {
+  return {
+    restrict: 'E',
+    replace: false,
+    transclude: true,
+    scope: {
+    },
+    templateUrl: 'js/directives/sideBasket/sideBasket.directive.html',
+    link: function (scope) {
+
+      scope.$watch(function(){
+        return userInfoService.user;
+      }, function(newValue){
+        if (newValue) {
+          scope.basketLength = userInfoService.user.basket.items.length;
+          scope.basketItems = userInfoService.user.basket.items;
+          scope.basketTotal = userInfoService.user.basket.total;
+
+
+          console.log('************');
+          console.log('basketItems:', scope.basketItems);
+          console.log('basketItems[0].product.title:', scope.basketItems[0].product.title);
+          console.log('total', userInfoService.user.basket.total);
+          console.log('basketItems[0].qty:', scope.basketItems[0].qty);
+        }
+      });
+
+    }
+  };
+}
+
+basketTotal.$inject = ['userInfoService'];
+
+angular
+  .module('MastersApp')
+  .directive('basketItems', basketTotal);
